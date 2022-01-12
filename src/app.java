7d211437a08f67d3165d2067222a34615ad26f26
13411 <13411193160a@gmail.com>
2022-01-12 13:48:49 -0800

    testing

--- src/app.java
+++ src/app.java
@@ -1,8 +1,43 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class app {
     public static void main(String[] args){
 
+        while (true){
+            int selection;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter the size of the problem: ");
+            int size = scanner.nextInt();
+
+            System.out.print("[1]Hill Climbing\n[2]Min Conflict\n[3]Exit\n");
+            selection = scanner.nextInt();
+
+            switch (selection){
+                case 1:
+                    HillClimbing hc = new HillClimbing(size);
+                    System.out.println("Hill Climbing");
+                    hc.randomlyRestart();
+                    break;
+                case 2:
+                    System.out.println("Min Conflict");
+                    System.out.println("Please enter the max step: ");
+                    int max = scanner.nextInt();
+
+                    MinConflict mc = new MinConflict(size,max);
+                    if(!mc.solve()){
+                        System.out.println("The search fail");
+                    }
+                    break;
+                case 3:
+                    return;
+                default:
+                    System.out.println("Invalid input");
+                    break;
+            }
+        }
+
+        /**
         HillClimbing hc = new HillClimbing(8);
         System.out.println("Hill Climbing");
         for(int j = 0 ; j < 50; j++){
@@ -28,6 +63,7 @@ public class app {
             System.out.println("The total time consuming: " + totalTime + "ns");
             System.out.print("\n\n\n");
         }
+         */
 
     }
 }
